e6acdb10194f31e17484c57b8b56a110632b1163
mdaleardc <ismailaliamot@gmail.com>
2025-01-27 22:06:12 +0600

    gsc changes

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router"
+import clsx from "clsx";
 
 
 const Books = ({filterData, isLoading}) => {
@@ -12,30 +13,42 @@ const Books = ({filterData, isLoading}) => {
                 <p className="h-4 bg-gray-400 rounded w-3/4 mt-2"></p>
                 <p className="h-4 bg-gray-400 rounded w-1/2 mt-1"></p>
       </div>
-      ))) : (filterData && (filterData.map((book, i) => (
+      ))) : (filterData && (filterData.filter(item=> item.pdfUrl).map((book, i) => (
       <div
   className="rounded-xl shadow-md text-center pb-2 bg-zinc-100 transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
   key={i}>
-  <img
+  {
+  book.thumbnailUrl.trim() ? (<img
     src={book.thumbnailUrl}
     alt="book's thumbnail"
-    className="rounded-md  mx-auto h-[200px] mt-2 object-cover"
-  />
+    className="rounded-md  mx-auto h-[200px] mt-1 object-cover"
+  />) : (
+  <div className='h-[200px] bg-zinc-300 m-1 rounded-md flex flex-col items-center justify-center'>
+  <p className='font-semibold'>Preview coming soon</p>
+  <p>Please stay connected</p>
+  </div>
+  )
+  }
   <h3 className="text-lg font-semibold text-gray-700">{book.subject.replace(/[_-]/g, " ")}</h3>
   <h2 className="text-base font-medium text-gray-800 px-2">{book.title.replace(/[_-]/g, " ").replace("Answers", "အဖြေစုံ")}</h2>
   <a
     href={book.pdfUrl}
     target="_blank"
     rel="noopener noreferrer"
-    className="inline-block mt-3 rounded-md bg-[#007bff] text-white py-1 px-4 transition-colors duration-300 hover:bg-[#0056b3]">
+    className={clsx(`inline-block mt-3 rounded-md text-white py-1 px-4 transition-colors duration-300`,
+    {
+      "bg-[#007bff] hover:bg-[#0056b3]" : book.pdfUrl.trim(),
+      "bg-zinc-500 pointer-events-none" : !book.pdfUrl.trim()
+    }
+    )}>
     Download PDF
   </a>
 </div>
       ))
       ))
     }
-    <Link to='/contact' className='bg-blue-500 text-white underline cursor-pointer hover:bg-blue-700 active:bg-violet-700 rounded-md w-fit py-1 px-2'>Contact Us</Link>
     </div>
+    <Link to='/contact' className='mx-10 mb-10 bg-blue-500 text-white underline cursor-pointer hover:bg-blue-700 active:bg-violet-700 rounded-md w-fit py-1 px-2'>Contact Us</Link>
     </>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -47,14 +47,18 @@ const Home = () => {
   
   return (
     <>
-    <h1 className='text-lg font-semibold text-[#333] text-center mt-3 mb-2'>Download All PDF Books For Free</h1>
+    <div className='fixed top-0 left-0 z-50 w-full bg-zinc-800 pb-3'>
+    <h1 className='text-lg font-semibold text-[#fff] text-center mt-3 mb-2'>Download All PDF Books For Free</h1>
     <form className='max-auto text-center flex flex-row items-center justify-center gap-4' onSubmit={handleSearch}>
-    <input type="text" placeholder="Search by grade" className="rounded-md w-2/5 sm:w-1/5 outline-none border focus:ring-[0.05rem] ring-[#02ff02]  py-1 px-2 text-1xl" value={searchQuery} onChange={(e)=> setSearchQuery(e.target.value)}/>
+    <input type="text" placeholder="Search books" className="rounded-md w-2/5 sm:w-1/5 outline-none border focus:ring-[0.05rem] ring-[#02ff02]  py-1 px-2 text-1xl" value={searchQuery} onChange={(e)=> setSearchQuery(e.target.value)}/>
     <button type="submit" className="inline-block rounded-md bg-[#007bff] text-white py-1 px-4 transition-colors duration-300 hover:bg-[#0056b3]">Search</button>
     </form>
+    </div>
+    <div className="mt-[5.6rem]">
     <Books filterData={filterData} isLoading={isLoading}/>
     <About />
     <Share />
+    </div>
     </>
     )
 }
