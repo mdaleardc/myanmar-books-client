e0c14a2d9782edcdc5ebabe733fbeba6103a1eb3
mdaleardc <ismailaliamot@gmail.com>
2025-02-03 16:15:51 +0600

    some style

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -7,7 +7,7 @@ const Books = ({filterData, isLoading}) => {
   const shuffledData = filterData && [...filterData].sort(()=>Math.random() - 0.5);
   return (
     <>
-    <div className='p-6 w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+    <div className='p-2 w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
     {
     isLoading ? (Array(6).fill("").map((_, index) => (
       <div key={index} className="animate-pulse bg-gray-300 rounded-md p-4 h-[280px] flex flex-col justify-center items-center">
@@ -17,13 +17,13 @@ const Books = ({filterData, isLoading}) => {
       </div>
       ))) : (filterData && (shuffledData.map((book, i) => (
       <div
-  className="rounded-xl shadow-md text-center pb-2 bg-zinc-100 transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+  className="rounded-md shadow-md text-center pb-2 bg-zinc-200 transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"
   key={i}>
   {
   book.thumbnailUrl.trim() ? (<img
     src={book.thumbnailUrl}
     alt="book's thumbnail"
-    className="rounded-md  mx-auto h-[200px] mt-1 object-cover"
+    className="rounded-lg  mx-auto w-11/12 h-[200px] mt-1 object-cover"
     loading="lazy"
   />) : (
   <div className='h-[200px] bg-zinc-300 m-3 rounded-md flex flex-col items-center justify-center'>
@@ -33,7 +33,7 @@ const Books = ({filterData, isLoading}) => {
   </div>
   )
   }
-  <h3 className="text-lg font-semibold text-gray-700">{book.subject.replace(/[_-]/g, " ")}</h3>
+  <h3 className="text-md font-semibold text-gray-700">{book.subject.replace(/[_-]/g, " ")}</h3>
   <h2 className="text-base font-medium text-gray-800 px-2">{book.title.replace(/[_-]/g, " ").replace("Answers", "အဖြေစုံ")}</h2>
   {
   book.clicks > 0 && (<p className='flex flex-row items-center justify-center text-gray-700'><MdDownloadForOffline size='25'/> {book.clicks}</p>)
