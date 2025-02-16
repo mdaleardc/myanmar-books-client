911597678b5a1e2a24f68b2f56aee4e65688aa6f
mdaleardc <ismailaliamot@gmail.com>
2025-02-16 22:00:13 +0600

    bg and fg color ratio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,9 @@ import { MdEmail } from "react-icons/md";
 
 const Contact = () => {
   return (
-    <div className="py-[2rem] mt-4 rounded-lg mb-3 bg-gradient-to-tr px-4 from-[#523] to-[#325] shadow-lg">
+    <div className="py-[2rem] mt-4 rounded-lg mb-3 bg-gradient-to-tr px-4 from-[#412] to-[#213] shadow-lg">
       <div className="max-w-3xl mx-auto text-center text-white">
-        <h1 className="text-[#0ef] text-3xl font-semibold mb-4">We&apos;d Love to Hear From You!</h1>
+        <h1 className="text-[#00bcd4] text-3xl font-semibold mb-4">We&apos;d Love to Hear From You!</h1>
         <p className="text-base tracking-wider mb-6">
           Have suggestions, feedback, or inquiries? Reach out by clicking on the options below.
         </p>
@@ -17,7 +17,7 @@ const Contact = () => {
               href="https://m.me/mdaismail.it"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-col items-center gap-2 p-4 rounded-md bg-blue-500 hover:bg-blue-600 transition duration-300 text-white w-32"
+              className="flex flex-col items-center gap-2 p-4 rounded-md bg-blue-600 hover:bg-blue-700 transition duration-300 text-white w-32"
             >
               <FaFacebookMessenger size="30" />
               <span className="text-sm font-semibold">Message Us</span>
@@ -25,7 +25,7 @@ const Contact = () => {
             <a
               aria-label="Email us via email"
               href="mailto:dismailait@gmail.com?subject=From: Myanmar School Books"
-              className="flex flex-col items-center gap-2 p-4 rounded-md bg-green-500 hover:bg-green-600 transition duration-300 text-white w-32"
+              className="flex flex-col items-center gap-2 p-4 rounded-md bg-green-600 hover:bg-green-700 transition duration-300 text-white w-32"
             >
               <MdEmail size="30" />
               <span className="text-sm font-semibold">Email Us</span>
