2d96338d7d45eb21086e9d02ec63771b28995d1e
mdaleardc <ismailaliamot@gmail.com>
2025-03-04 16:37:38 +0600

    color contrast change

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -62,10 +62,10 @@ const Books = ({ filterData, isLoading }) => {
       key={grade}
       onClick={() => setSelectedGrade(grade)}
       className={`text-sx sm:text-xl font-semibold bg-[#0F008A] rounded-full transition flex items-center justify-between px-2 py-1 ${
-        selectedGrade === grade ? "bg-[#08004C] text-green-500" : "text-white"
+        selectedGrade === grade ? "bg-[#08004C] text-[gold]" : "text-white"
       }`}
     >
-      {grade.replace(/_/g, "-")} <span className='flex gap-2'><span className={`flex items-center gap-1 text-sm ${selectedGrade === grade ? "text-green-500":"text-white"} `}><IoBookSharp size='15'/> {bookCount}</span> <span className='hidden sm:flex items-center gap-1 text-xs'> <MdDownloadForOffline size='15'/> {totalClicks}</span></span>
+      {grade.replace(/_/g, "-")} <span className='flex gap-2'><span className={`flex items-center gap-1 text-sm ${selectedGrade === grade ? "text-[gold]":"text-white"} `}><IoBookSharp size='15'/> {bookCount}</span> <span className='hidden sm:flex items-center gap-1 text-xs'> <MdDownloadForOffline size='15'/> {totalClicks}</span></span>
     </button>
   );
 })}
