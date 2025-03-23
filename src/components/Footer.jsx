70887729bcf14970b35011af5dfb51bb712161ae
mdaleardc <ismailaliamot@gmail.com>
2025-03-23 23:22:20 +0600

    small change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 text-center">
   <p className="text-sm">
-    © {new Date().getFullYear()} Mohammed Ismail. All rights reserved.
+    © {new Date().getFullYear()} Myanmar Books Hub. All rights reserved.
   </p>
   <p className="text-sm">
     Created with ❤️ by{" "}
